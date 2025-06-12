476f4579fe7b5bccd1be5f1c9b428bca599d2cd9
affanengi <mohammedaffanrazvi604@gmail.com>
2025-06-12 12:20:45 +0530

    First commit

---
+++ func.js
@@ -0,0 +1,26 @@
+//functions are used to perform various operations on the data
+//They  allow us to manipulate the data in a more efficient way
+
+//functios are used always like 
+//Declaration: functionName(arg1, arg2, ...)
+//Call: functionName(arg1, arg2, ...)
+
+function add(a, b) {
+    console.log("Sum is", a + b);
+}
+//Function call
+add(1, 2); 
+
+//Functions with parameters
+function addWithParams(a, b) {
+    console.log("Sum is", a + b);
+}
+//Function call
+let sum = addWithParams(3, 4);
+
+function multiplyWithParams(a, b, c) {
+    console.log("Product is", a * b * c);
+}
+//Function call
+let product = multiplyWithParams(2, 3, 4);
+
